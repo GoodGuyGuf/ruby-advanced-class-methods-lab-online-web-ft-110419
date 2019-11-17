62601665e81149ef6fc2fdc304450782847934fd
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-17 18:02:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,61 @@
 class Song
+
   attr_accessor :name, :artist_name
   @@all = []
 
   def self.all
     @@all
+  end
+
+  def self.create
+    song = self.new
+    @@all << song
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    return song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    return song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    find = self.find_by_name(name)
+    if find.nil?
+      create = self.create_by_name(name)
+      return create
+    else
+      return find
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename_format)
+    song = self.new
+
+  end
+
+  def self.create_from_filename(filename_format)
+    song = self.new
+
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
   def save
